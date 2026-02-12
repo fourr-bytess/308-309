f6e48a0dccbf01577dc33b623f1d55c45c266eb8
kaitlynbresnan <kaitlynbresnan@gmail.com>
2026-02-11 17:02:02 -0800

    band schema and first part of band db

---
+++ packages/express-backend/band-services.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import bandModel from "./band.js";
+
+function getBands(name, member_names, genres, locations, price_range) {
+    let promise;
+    const query = {};
+    if (name) {
+        query.name = name.toLowerCase();
+    }
+    if (member_names) {
+        query.member_names = { $in: member_names.map(m => m.toLowerCase())};
+    }
+    if (genres?.length) {
+        query.genres = { $in: genres.map(g => g.toLowerCase())};
+    }
+    if (locations?.length) {
+        query.locations = { $in: locations.map(l => l.toLowerCase())};
+    }
+    if (price_range?.length === 2) {
+        query.price = {
+            $gte: price_range[0],
+            $lte: price_range[1]
+        };
+    }
+    promise = bandModel.find(query);
+    return promise;
+}

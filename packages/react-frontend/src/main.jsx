eb2bcbf1b7fda655884375e5b1b08d5a92f969e8
kaitlynbresnan <kaitlynbresnan@gmail.com>
2026-02-25 18:55:34 -0800

    Update main.jsx

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
+// need to connect this to app.js
 import App from './app.js'
 
 createRoot(document.getElementById('root')).render(
